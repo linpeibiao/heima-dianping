a30616ad154afe61fe8893e7eed83b3080712511
linxiaohu <205838147@qq.com>
2022-11-02 20:16:47 +0800

    finish project init

---
+++ src/main/java/com/xiaohu/heimadianping/HeiMaDianPingApplication.java
@@ -0,0 +1,17 @@
+package com.xiaohu.heimadianping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xiaohu
+ * @date 2022/11/02/ 20:02
+ * @description
+ */
+
+@SpringBootApplication
+public class HeiMaDianPingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HeiMaDianPingApplication.class, args);
+    }
+}
